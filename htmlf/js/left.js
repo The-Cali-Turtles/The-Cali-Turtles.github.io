2cbc30a8ddf67e958bf178931e7854216338b2a1
The-Cali-Turtles <elijah.littellsivley@portageps.org>
2020-03-13 09:48:12 -0400

    changes

--- htmlf/js/left.js
+++ htmlf/js/left.js
@@ -1,6 +1,18 @@
 function moveImgLeft() {
 
-    var currentImg = document.getElementById("select");
-    currentImg.style.background = "green";
+    var current = document.getElementById("select");
+    var next = current.nextElementSibling;
+    var button = document.querySelector("right-button");
+    var first = document.getElementById("first");
+
+    if(next == button) {
+        current.removeAttribute("id");
+        first.id = "select";
+    } 
+    
+    current.removeAttribute("id");
+    current.style.background = "lightgray";
+    next.id = "select";
+    current.style.background = "white";
 
 }
